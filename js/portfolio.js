66f0f78fc5c787cbd5adbf93a0009647b56cff03
LilenAmado <65418208+LilenAmado@users.noreply.github.com>
2020-10-26 04:01:26 -0300

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,12 +9,13 @@ audiovisual_boton.click(()=>{
     audiovisual_galery.show();
     //desarrollo_galery.fadeTo('slow',0);
     audiovisual_galery.fadeTo('slow',1);
+    aclaracion.show();
 });
 
 desarrollo_boton.click(()=>{
     desarrollo_galery.show();
     audiovisual_galery.hide();
+    aclaracion.hide();
     //audiovisual_galery.fadeTo('slow',0);
     desarrollo_galery.fadeTo('slow',1);
 });
-    
