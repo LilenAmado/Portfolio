f5ae69b5f7460d5562cd51b4b0bc5da80c7ead4f
LilenAmado <65418208+LilenAmado@users.noreply.github.com>
2020-12-04 03:07:05 -0300

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,15 @@
 //Portfolio
 let desarrollo_galery = $('#desarrollo_galery');
 let audiovisual_galery = $('#audiovisual_galery');
+let design_galery = $('#design_galery');
 let desarrollo_boton = $('#desarrollo');
 let audiovisual_boton = $('#audiovisual');
-let aclaracion = $('#aclaracion');
+let design_boton = $('#design');
+let aclaracion = $('#aclaracion')
 
 audiovisual_boton.click(()=>{
     desarrollo_galery.hide();
+    design_galery.hide();
     audiovisual_galery.show();
     //desarrollo_galery.fadeTo('slow',0);
     audiovisual_galery.fadeTo('slow',1);
@@ -16,7 +19,17 @@ audiovisual_boton.click(()=>{
 desarrollo_boton.click(()=>{
     desarrollo_galery.show();
     audiovisual_galery.hide();
+    design_galery.hide();
     aclaracion.hide();
     //audiovisual_galery.fadeTo('slow',0);
     desarrollo_galery.fadeTo('slow',1);
-});
+}); 
+
+design_boton.click(()=>{
+    design_galery.show();
+    desarrollo_galery.hide();
+    audiovisual_galery.hide();
+    aclaracion.hide();
+    //audiovisual_galery.fadeTo('slow',0);
+    design_galery.fadeTo('slow',1);
+}); 
